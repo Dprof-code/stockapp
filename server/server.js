85957d9103bd46e6d43485cc19d73a443c6dfb3b
dprof-code <olawaleade15@gmail.com>
2024-04-25 18:34:29 -0700

    setup client and database

--- server/server.js
+++ server/server.js
@@ -1,12 +1,26 @@
-const express = require('express')
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express'
+import mysql from 'mysql'
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
-app.use(express.json());
+
+const db = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "stockdb"
+})
+
+app.get('/', (req, res) => {
+    const sql = "SELECT * FROM stock";
+    db.query(sql, (err, result) => {
+        if(err) return res.json({Message: "Error inside server"});
+        return res.json(result);
+    })
+})
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
