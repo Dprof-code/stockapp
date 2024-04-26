015ec0df02c02069b9ab6350a2c266289265db0b
dprof-code <olawaleade15@gmail.com>
2024-04-26 03:44:34 -0700

    added stock data

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,45 @@
 import logo from './logo.svg';
 import './App.css';
+import Table from 'react-bootstrap/Table';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th>#</th>
+          <th>STOCK ID</th>
+          <th>STOCK NAME</th>
+          <th>STOCK TICKER SYMBOL</th>
+          <th>COMPANY NAME</th>
+          <th>SECTOR</th>
+          <th>INDUSTRY</th>
+          <th>EXCHANGE</th>
+          <th>INDUSTRY</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>1</td>
+          <td>Mark</td>
+          <td>Otto</td>
+          <td>@mdo</td>
+        </tr>
+        <tr>
+          <td>2</td>
+          <td>Jacob</td>
+          <td>Thornton</td>
+          <td>@fat</td>
+        </tr>
+        <tr>
+          <td>3</td>
+          <td colSpan={2}>Larry the Bird</td>
+          <td>@twitter</td>
+        </tr>
+      </tbody>
+    </Table>
   );
 }
+
 
 export default App;
