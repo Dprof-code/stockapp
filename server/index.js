6bffc70a7e7454b32f361b8e99cb31b2a81fce45
dprof-code <olawaleade15@gmail.com>
2024-05-03 04:22:10 -0700

    database setup

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mysql from 'mysql'
 import cors from 'cors';
+import fetch from 'node-fetch';
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -15,16 +16,28 @@ const db = mysql.createConnection({
 })
 
 app.get('/', (req, res) => {
-    const sql = "SELECT * FROM stock";
-    db.query(sql, (err, result) => {
-        if (err) {
-            console.log(err);
-            return res.json({Message: "Error inside server"});
-        } else {
-            return res.json(result);
-        }
-    })
-})
+    // Fetching data from the REST API'
+    fetch('https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/2023-01-09?adjusted=true&resultsCount=4&apiKey=9IrKSK00IMXNYApTpspYr42q3aE9wU8W')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const stocks = data.results;
+            const sql = "INSERT INTO stock (symbol, volume, volume_weighted, open_price, close_price, high_price, low_price, timestamp, trades) VALUES ?";
+            const values = stocks.map(stock => [stock.T, stock.v, stock.vw, stock.o, stock.c, stock.h, stock.l, stock.t, stock.n]); // Assuming 'symbol' and 'price' are the properties of each stock object
+            db.query(sql, [values], (err, result) => {
+                if (err) {
+                    console.log(err);
+                    return res.json({ message: "Error while inserting data into database" });
+                } else {
+                    return res.json({ message: "Data inserted successfully" });
+                }
+            });
+        })
+        .catch(error => {
+            console.log(error);
+            return res.json({ message: "Error fetching data from API" });
+        });
+});
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
